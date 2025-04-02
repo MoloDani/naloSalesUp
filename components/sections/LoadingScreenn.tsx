0d2b143a44c7b1f1a26ad27f076ca1711a641512
MoloDani <126819876+MoloDani@users.noreply.github.com>
2025-04-03 00:03:01 +0300

    test3

--- components/sections/LoadingScreenn.tsx
+++ components/sections/LoadingScreenn.tsx
@@ -8,11 +8,12 @@ const LoadingScreen: FC<LoadingScreenProps> = ({ onFinish }) => {
   return (
     <div className="fixed inset-0 flex items-center w-full h-full justify-center bg-black z-50">
       <video
-        src="/assets/Crown_just_green_Loading_Animation.mp4"
+        src="/assets/Crown_just_green_Loading_Animation_v2.mp4"
         autoPlay
         muted
         onEnded={onFinish}
-        className="w-[80vw] max-w-[300px] h-auto object-cover"
+        playsInline
+        className="w-[80vw] h-auto object-cover"
       />
     </div>
   );
