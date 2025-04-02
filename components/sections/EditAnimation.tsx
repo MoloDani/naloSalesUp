b1a148f8e3513a2b3adbf80fd02778d2972f86c1
MoloDani <126819876+MoloDani@users.noreply.github.com>
2025-04-02 23:30:13 +0300

    maybe problem solved

--- components/sections/EditAnimation.tsx
+++ components/sections/EditAnimation.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const EditAnimation: React.FC = () => {
+const EditAnimation = () => {
+  const [isSafari, setIsSafari] = useState(false);
+
+  useEffect(() => {
+    // Function to check for browser support
+    setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+    console.log(isSafari);
+  }, []);
+
   return (
     <section
       id="edit-animation"
@@ -17,17 +25,22 @@ const EditAnimation: React.FC = () => {
           preload="auto"
           controls={false} // Ensures no controls are displayed
           onError={() => alert("Sorry, the video couldn't load.")}
+          poster="/assets/video-placeholder.png" // Placeholder image while loading
         >
-          {/* WebM for Chrome, Firefox, Edge */}
-          <source src="/assets/LQ5_Timeline_Animation.webm" type="video/webm" />
-
-          {/* MOV for Safari */}
-          <source
-            src="/assets/LQ5_Timeline_Animation_1.mov"
-            type="video/quicktime"
-          />
+          {isSafari ? (
+            <source
+              src="/assets/LQ5_Timeline_Animation_1.mov"
+              type="video/quicktime"
+            />
+          ) : (
+            <source
+              src="/assets/LQ5_Timeline_Animation.webm"
+              type="video/webm"
+            />
+          )}
+          {/* Fallback content if video fails to load */}
+          Sorry, your browser doesn't support the video tag.
         </video>
-
         {/* Text Positioned at the Bottom, Over the Video */}
         <div className="absolute bottom-0 w-full text-center p-4 z-10">
           <h1 className="text-white text-[2rem] lg:text-[3rem] font-bold">
