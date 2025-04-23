a38760e6067192540f026dca352646a1112f8e74
MoloDani <126819876+MoloDani@users.noreply.github.com>
2025-04-23 17:56:38 +0300

    test V2

--- components/utils/NoBgVideo.tsx
+++ components/utils/NoBgVideo.tsx
@@ -8,19 +8,21 @@ const NoBgVideo: React.FC<NoBgVideoProps> = ({ srcBase }) => {
   const [isSafari, setIsSafari] = useState(false);
 
   useEffect(() => {
+    // Check if the browser is Safari
     const detectedSafari =
       /Safari/.test(navigator.userAgent) &&
       /Apple Computer/.test(navigator.vendor);
     setIsSafari(detectedSafari);
   }, []);
 
-  const videoSrc = `${srcBase}.webm`; // Force using .webm for compatibility
-  const videoType = "video/webm"; // Use .webm for all browsers
+  // Choose the video file based on the browser
+  const videoSrc = isSafari ? `${srcBase}.mov` : `${srcBase}.webm`;
+  const videoType = isSafari ? "video/quicktime" : "video/webm"; // Safari needs quicktime type for .mov
 
   return (
     <div className="relative">
       <video
-        key={videoSrc}
+        key={videoSrc} // This ensures the video is re-rendered when `srcBase` changes
         className="w-[90vw] lg:w-[35vw] h-auto align-middle -mt-80 mb-10 lg:mb-0"
         autoPlay
         loop
