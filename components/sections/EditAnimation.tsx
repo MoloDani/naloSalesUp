f36da3d4a8f71c08bdeea272d561377cdcb7aea5
MoloDani <126819876+MoloDani@users.noreply.github.com>
2025-04-23 18:06:14 +0300

    verify old logic

--- components/sections/EditAnimation.tsx
+++ components/sections/EditAnimation.tsx
@@ -2,6 +2,14 @@ import React, { useEffect, useState } from "react";
 import NoBgVideo from "../utils/NoBgVideo";
 
 const EditAnimation = () => {
+  const [isSafari, setIsSafari] = useState(false);
+
+  useEffect(() => {
+    // Function to check for browser support
+    setIsSafari(/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
+    console.log(isSafari);
+  }, []);
+
   return (
     <section
       id="edit-animation"
@@ -9,7 +17,31 @@ const EditAnimation = () => {
     >
       {/* Video Container */}
       <div className="flex flex-col items-center justify-center w-full relative">
-        <NoBgVideo srcBase="/assets/LQ5_Timeline_animation" />
+        <video
+          className="w-[90vw] lg:w-[35vw] h-auto align-middle -mt-80 mb-10 lg:mb-0"
+          autoPlay
+          loop
+          muted
+          playsInline
+          preload="auto"
+          controls={false} // Ensures no controls are displayed
+          onError={() => alert("Sorry, the video couldn't load.")}
+          poster="/assets/video-placeholder.png" // Placeholder image while loading
+        >
+          {isSafari ? (
+            <source
+              src="/assets/LQ5_Timeline_Animation_1.mov"
+              type="video/quicktime"
+            />
+          ) : (
+            <source
+              src="/assets/LQ5_Timeline_Animation.webm"
+              type="video/webm"
+            />
+          )}
+          {/* Fallback content if video fails to load */}
+          Sorry, your browser doesn't support the video tag.
+        </video>
         {/* Text Positioned at the Bottom, Over the Video */}
         <div className="absolute bottom-0 w-full text-center p-4 z-10">
           <h1 className="text-white text-[2rem] lg:text-[3rem] font-bold">
